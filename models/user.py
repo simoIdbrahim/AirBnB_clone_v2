1451079e6a6387b76e6d91c10970f13dd03b0066
imadovetch <ibouderoua63@gmail.com>
2023-12-19 22:41:35 +0100

    try other code just for test

--- models/user.py
+++ models/user.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
-"""User class definition."""
-
-from sqlalchemy.ext.declarative import declarative_base
+"""'User' class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
+from os import getenv
 
 
 class User(BaseModel, Base):
-    """User class for storing user information.
-
-    Attributes:
-        email (str): Email address of the user.
-        password (str): Password for user login.
-        first_name (str): First name of the user.
-        last_name (str): Last name of the user.
-        places (relationship): One-to-many relationship with Place.
-        reviews (relationship): One-to-many relationship with Review.
+    """represents a user for a MySQL database
+    Inherits from  the SQLAlchemy Base and links to the MySQL table users
     """
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship('Place', cascade="all,delete", backref="user")
+        reviews = relationship('Review', cascade="all,delete", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
