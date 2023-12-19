8da88241de56210ccdfafbd377e485ad72e9e92b
imadovetch <ibouderoua63@gmail.com>
2023-12-19 20:14:00 +0100

    fie storage update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""Ce module définit une classe pour gérer le stockage de fichiers pour le clone hbnb."""
 import json
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    """Cette classe gère le stockage des modèles hbnb au format JSON."""
+
     __file_path = 'file.json'
     __objects = {}
 
     def all(self):
-        """Returns a dictionary of models currently in storage"""
+        """Retourne un dictionnaire des modèles actuellement en stockage."""
         return FileStorage.__objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        """Ajoute un nouvel objet au dictionnaire de stockage."""
+        key = f"{obj.to_dict()['__class__']}.{obj.id}"
+        self.all().update({key: obj})
 
     def save(self):
-        """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+        """Enregistre le dictionnaire de stockage dans un fichier."""
+        with open(FileStorage.__file_path, 'w') as file:
+            temp = {key: val.to_dict() for key, val in FileStorage.__objects.items()}
+            json.dump(temp, file)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """Charge le dictionnaire de stockage à partir du fichier."""
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -36,15 +35,15 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+            with open(FileStorage.__file_path, 'r') as file:
+                temp = json.load(file)
+                FileStorage.__objects = {
+                    key: classes[val['__class__']](**val) for key, val in temp.items()
+                }
         except FileNotFoundError:
             pass
