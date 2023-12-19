d0bf1e7b10f686882e82f4b936a31ea65e0ce5f2
imadovetch <ibouderoua63@gmail.com>
2023-12-19 23:11:53 +0100

    test imad

--- models/user.py
+++ models/user.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-"""User class definition."""
+"""Place class definition."""
 
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
+
+
+class Place(BaseModel, Base):
+    """Place class for storing place information."""
+    __tablename__ = "places"
+    # Define Place class columns and attributes here
+
+
+class Review(BaseModel, Base):
+    """Review class for storing review information."""
+    __tablename__ = "reviews"
+    # Define Review class columns and attributes here
 
 
 class User(BaseModel, Base):
-    """User class for storing user information.
-
-    Attributes:
-        email (str): Email address of the user.
-        password (str): Password for user login.
-        first_name (str): First name of the user.
-        last_name (str): Last name of the user.
-        places (relationship): One-to-many relationship with Place.
-        reviews (relationship): One-to-many relationship with Review.
-    """
+    """User class for storing user information."""
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
+    places = relationship("Place", cascade='all, delete, delete-orphan', backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan', backref="user")
