eb5cba57a5ef046286eb13b3f8fc66c479c0c994
imadovetch <ibouderoua63@gmail.com>
2023-12-19 23:08:55 +0100

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,11 +2,11 @@
 """Ce module définit une classe pour gérer le stockage de fichiers pour le clone hbnb."""
 import json
 from models.base_model import BaseModel
-from models.user import User
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 from models.place import Place
+from models.user import User
 from models.state import State
 
 
