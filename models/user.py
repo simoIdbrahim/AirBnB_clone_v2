0f417142c8e758bc996e90ed51a0203660071ffe
imadovetch <ibouderoua63@gmail.com>
2023-12-19 23:16:28 +0100

    test imad

--- models/user.py
+++ models/user.py
@@ -1,30 +1,20 @@
-#!/usr/bin/python3
-"""Place class definition."""
-
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-
+from sqlalchemy import ForeignKey
 
 class Place(BaseModel, Base):
     """Place class for storing place information."""
     __tablename__ = "places"
-    # Define Place class columns and attributes here
-
-
-class Review(BaseModel, Base):
-    """Review class for storing review information."""
-    __tablename__ = "reviews"
-    # Define Review class columns and attributes here
-
+    
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    # Add other columns for Place class
 
 class User(BaseModel, Base):
     """User class for storing user information."""
     __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True)
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    
     places = relationship("Place", cascade='all, delete, delete-orphan', backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan', backref="user")
