aba91fab9cf45c76f0f8ea91db9d703f0d5f33f3
imadovetch <ibouderoua63@gmail.com>
2023-12-19 22:43:13 +0100

    try other code just for test

--- models/user.py
+++ models/user.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
-"""'User' class"""
+"""User class definition."""
+
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from os import getenv
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel, Base):
-    """represents a user for a MySQL database
-    Inherits from  the SQLAlchemy Base and links to the MySQL table users
+    """User class for storing user information.
+
+    Attributes:
+        email (str): Email address of the user.
+        password (str): Password for user login.
+        first_name (str): First name of the user.
+        last_name (str): Last name of the user.
+        places (relationship): One-to-many relationship with Place.
+        reviews (relationship): One-to-many relationship with Review.
     """
     __tablename__ = "users"
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship('Place', cascade="all,delete", backref="user")
-        reviews = relationship('Review', cascade="all,delete", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
