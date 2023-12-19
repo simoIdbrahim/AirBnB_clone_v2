b2b0ac03df1e9cab458ed36be221e3da67ec657f
imadovetch <ibouderoua63@gmail.com>
2023-12-19 20:23:55 +0100

    hot fixes filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,37 +5,42 @@ import json
 
 class FileStorage:
     """Cette classe gère le stockage des modèles hbnb au format JSON"""
-    __file_path = 'file.json'
+
     __objects = {}
+    __file_path = 'file.json'
 
     def all(self):
         """Retourne un dictionnaire des modèles actuellement en stockage"""
+
         return FileStorage.__objects
 
     def new(self, obj):
         """Ajoute un nouvel objet au dictionnaire de stockage"""
+
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
         """Enregistre le dictionnaire de stockage dans un fichier"""
+
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
+
+            for keyword, value in temp.items():
+                temp[keyword] = value.to_dict()
             json.dump(temp, f)
 
     def reload(self):
         """Charge le dictionnaire de stockage à partir du fichier"""
         from models.base_model import BaseModel
-        from models.user import User
+        from models.review import Review
         from models.place import Place
-        from models.state import State
+        from models.user import User
         from models.city import City
         from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
+        from models.state import State
+        
+        avilable = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
                     'Review': Review
@@ -44,7 +49,7 @@ class FileStorage:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
-                for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                for keyword, value in temp.items():
+                        self.all()[keyword] = avilable[value['__class__']](**value)
         except FileNotFoundError:
             pass
