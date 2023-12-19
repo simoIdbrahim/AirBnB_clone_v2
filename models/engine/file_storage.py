95abdfae34974c5347c0d04689b80c7c743087b1
imadovetch <ibouderoua63@gmail.com>
2023-12-19 20:16:21 +0100

    fie storage hot fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""Ce module définit une classe pour gérer le stockage de fichiers pour le clone hbnb"""
+"""This module defines a class to manage file storage for hbnb clone"""
 import json
 
 
 class FileStorage:
-    """Cette classe gère le stockage des modèles hbnb au format JSON"""
+    """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
     def all(self):
-        """Retourne un dictionnaire des modèles actuellement en stockage"""
+        """Returns a dictionary of models currently in storage"""
         return FileStorage.__objects
 
     def new(self, obj):
-        """Ajoute un nouvel objet au dictionnaire de stockage"""
+        """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
-        """Enregistre le dictionnaire de stockage dans un fichier"""
+        """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
@@ -26,7 +26,7 @@ class FileStorage:
             json.dump(temp, f)
 
     def reload(self):
-        """Charge le dictionnaire de stockage à partir du fichier"""
+        """Loads storage dictionary from file"""
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
