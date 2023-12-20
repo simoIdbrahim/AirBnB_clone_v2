f6cd560a5130b37e58592f425689e15b075641c4
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-12-20 05:50:34 -0800

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" test """
+""" """
 import json
 from models.base_model import BaseModel
 from models.amenity import Amenity
@@ -11,53 +11,4 @@ from models.user import User
 
 
 class FileStorage:
-    """ 
-    """
-
-    __file_path = "file.json"
-    __objects = {}
-
-    def all(self, cls=None):
-        """
-        """
-        if cls is not None:
-            if type(cls) == str:
-                cls = eval(cls)
-            cls_dict = {}
-            for k, v in self.__objects.items():
-                if type(v) == cls:
-                    cls_dict[k] = v
-            return cls_dict
-        return self.__objects
-
-    def new(self, obj):
-        """ """
-        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
-
-    def save(self):
-        """ """
-        odict = {o: self.__objects[o].to_dict() for o in self.__objects.keys()}
-        with open(self.__file_path, "w", encoding="utf-8") as f:
-            json.dump(odict, f)
-
-    def reload(self):
-        """ """
-        try:
-            with open(self.__file_path, "r", encoding="utf-8") as f:
-                for o in json.load(f).values():
-                    name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(name)(**o))
-        except FileNotFoundError:
-            pass
-
-    def delete(self, obj=None):
-        """ """
-        try:
-            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
-        except (AttributeError, KeyError):
-            pass
-
-    def close(self):
-        """  """
-        self.reload()
+    pass
