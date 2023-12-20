6f0f5bf11657e83f5e9e941e328c549eec432821
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-12-20 12:46:02 -0800

    city test

--- models/city.py
+++ models/city.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""This is the city class"""
-from sqlalchemy.ext.declarative import declarative_base
+""" City Module for HBNB project """
+from sqlalchemy import Column, ForeignKey, String
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
-from models.place import Place
 
 
 class City(BaseModel, Base):
-    """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
+    """
+    >>City inherits from BaseModel and Base (respect the order)
+    >>class attribute __tablename__ -
+            represents the table name, cities
+    >>class attribute name
+            represents a column containing a string (128 characters)
+            cant be null
+    >>class attribute state_id
+            represents a column containing a string (60 characters)
+            cant be null
+            is a foreign key to states.id
     """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="cities")
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
