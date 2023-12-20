363af06d9e329d2a760c132804863b837e46a560
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-12-20 04:54:57 -0800

    add

--- models/place.py
+++ models/place.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" test """
+""" Definition of the Place class and its attributes """
+
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
@@ -7,7 +8,7 @@ from sqlalchemy.orm import relationship
 from os import getenv
 import models
 
-
+# Define a table for the many-to-many relationship between Place and Amenity
 place_amenity = Table("place_amenity", Base.metadata,
                       Column("place_id", String(60),
                              ForeignKey("places.id"),
@@ -18,12 +19,16 @@ place_amenity = Table("place_amenity", Base.metadata,
                              primary_key=True,
                              nullable=False))
 
+# Define the Place class
+
 
 class Place(BaseModel, Base):
-    """ 
-      test
-    """
+    """ Class representing a place with various attributes """
+
+    # Define the table name
     __tablename__ = "places"
+
+    # Define columns and their data types
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
@@ -36,6 +41,7 @@ class Place(BaseModel, Base):
     longitude = Column(Float)
     amenity_ids = []
 
+    # Define relationships based on the storage type
     if getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review", cascade='all, delete, delete-orphan',
                                backref="place")
@@ -44,29 +50,22 @@ class Place(BaseModel, Base):
                                  viewonly=False,
                                  back_populates="place_amenities")
     else:
+        # Define properties for non-database storage
         @property
         def reviews(self):
-            """ test """
+            """Returns a list of reviews associated with the place"""
             var = models.storage.all()
-            lista = []
-            result = []
-            for key in var:
-                review = key.replace('.', ' ')
-                review = shlex.split(review)
-                if (review[0] == 'Review'):
-                    lista.append(var[key])
-            for elem in lista:
-                if (elem.place_id == self.id):
-                    result.append(elem)
-            return (result)
+            lista = [var[key] for key in var if key.replace('.', ' ').split()[
+                0] == 'Review']
+            return [elem for elem in lista if elem.place_id == self.id]
 
         @property
         def amenities(self):
-            """ test """
+            """Returns a list of amenity ids associated with the place"""
             return self.amenity_ids
 
         @amenities.setter
         def amenities(self, obj=None):
-            """ test """
-            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+            """Appends amenity ids to the attribute"""
+            if isinstance(obj, Amenity) and obj.id not in self.amenity_ids:
                 self.amenity_ids.append(obj.id)
