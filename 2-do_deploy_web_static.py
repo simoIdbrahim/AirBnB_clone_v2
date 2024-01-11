f53aaa04e4351deb2dd9c8353b055175441a2227
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-01-11 12:14:49 -0800

    add

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,31 +1,49 @@
 #!/usr/bin/python3
-""" script (based on the file 1-pack_web_static.py) that distributes
-an archive to your web servers, using the function do_deploy """
-from os.path import exists, splitext
-from fabric.api import *
+""" script (based on the file 1-pack_web_static.py) that distributes an
+archive to your web servers, using the function do_deploy """
+from datetime import datetime
+from os import path, mkdir
+from fabric.api import local, put, run, env
 
-env.hosts = ['54.87.180.223', '52.23.245.155']
+env.hosts = ['3.85.196.229', '34.207.221.84']
 
 
-@task
+def do_pack():
+    """ do_pack function """
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    file = "versions/web_static_{}.tgz".format(date)
+
+    if not path.exists("versions"):
+        mkdir("versions")
+
+    local("tar -cvzf {} web_static".format(file))
+
+    if path.exists(file):
+        return file
+    else:
+        return None
+
+
 def do_deploy(archive_path):
-    """ func deply archive"""
-
-    if not exists(archive_path):
+    """ do_deploy function """
+    if not path.exists(archive_path):
         return False
 
-    arch_name = archive_path.split('/')[1]
-    file_name = splitext(arch_name)[0]
-    tmp_path = "/tmp/{}".format(arch_name)
-    data_path = "/data/web_static/releases/{}".format(file_name)
-
-    put(archive_path, '/tmp/')
-    run('mkdir -p {}'.format(data_path))
-    run('tar -xzf {}  -C {}'.format(tmp_path, data_path))
-    run('rm {}'.format(tmp_path))
-    run('mv {}/web_static/* {}/'.format(data_path, data_path))
-    run('rm -rf {}/web_static'.format(data_path))
-    run('rm -rf /data/web_static/current')
-    run('ln -s {} /data/web_static/current'.format(data_path))
-    print("New version deployed!")
-    return True
+    try:
+        put(archive_path, "/tmp/")
+        file_name = archive_path.split("/")[-1]
+        name = file_name.split('.')[0]
+        dir_release = "/data/web_static/releases/{}".format(name)
+        run("mkdir -p {}".format(dir_release))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, dir_release))
+        run("rm /tmp/{}".format(file_name))
+        run('mv {0}/web_static/* {0}/'.format(dir_release))
+        run('rm -rf {}/web_static'.format(dir_release))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(dir_release))
+        print("New version deployed!")
+        return True
+    except Exception:
+        print("No new version deployed!")
+        return False
